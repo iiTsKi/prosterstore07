26492954365f3d469b0624b748bb44124cd4771d
ItsArthurPC <itski.arthur@gmail.com>
2025-08-24 16:25:51 +0800

    removingDelayInLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,11 @@
 import Footer from "@/components/footer";
 import Header from "@/components/shared/header";
 
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  await delay(2000);
   return (
     <div className="flex h-screen flex-col">
       <Header />
